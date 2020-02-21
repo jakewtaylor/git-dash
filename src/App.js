e11a2d5de95a0429237a07a1d1a5294f6e5cd7e7
Jake Taylor <me@jaketaylor.co>
2020-02-21 02:01:09 +0000

    Remove token... oops

--- src/App.js
+++ src/App.js
@@ -6,14 +6,12 @@ import { RepoPicker } from './components/RepoPicker';
 import { ConfigController } from './components/ConfigController/ConfigController';
 import { TokenGate } from './components/TokenGate';
 
-const token = '514efa1c3afb28538a89d74a2d19ce9ef789baea';
-
 export const App = () => {
   return (
     <ConfigController>
       <Layout>
         <TokenGate>
-          <Apollo token={token}>
+          <Apollo>
             <RepoPicker />
             {/* <Test /> */}
           </Apollo>
