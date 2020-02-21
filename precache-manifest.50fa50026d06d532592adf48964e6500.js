29957726231997beace404093c01ec38e1d8e301
Jake Taylor <me@jaketaylor.co>
2020-02-21 01:55:49 +0000

    Updates

---
+++ precache-manifest.50fa50026d06d532592adf48964e6500.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91abf8184fb9a49df3451d9042381b48",
+    "url": "/git-dash/index.html"
+  },
+  {
+    "revision": "574a1eee62102382815e",
+    "url": "/git-dash/static/css/main.636bd381.chunk.css"
+  },
+  {
+    "revision": "4dd484c9700addc9eb7c",
+    "url": "/git-dash/static/js/2.4c1bdcb5.chunk.js"
+  },
+  {
+    "revision": "de1cedeacf4641d6277b42334cfacb1d",
+    "url": "/git-dash/static/js/2.4c1bdcb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "574a1eee62102382815e",
+    "url": "/git-dash/static/js/main.a62a471b.chunk.js"
+  },
+  {
+    "revision": "8ecbc2b9b3732320ba1d",
+    "url": "/git-dash/static/js/runtime-main.01556bde.js"
+  }
+]);
