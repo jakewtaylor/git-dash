f1c45e16fef13612c0c1933b86a1e45d20594ff6
Jake Taylor <jake.taylor@pickr.works>
2020-02-26 15:35:10 +0000

    Fix crash on pending reviews

--- src/components/PullRequests/StatusIcon.js
+++ src/components/PullRequests/StatusIcon.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ChatBubbleRoundedIcon from '@material-ui/icons/ChatBubbleRounded';
 import RateReviewRoundedIcon from '@material-ui/icons/RateReviewRounded';
 import CheckRoundedIcon from '@material-ui/icons/CheckRounded';
+import AccessTimeRoundedIcon from '@material-ui/icons/AccessTimeRounded';
 import { useStyles } from './StatusIcon.styles';
 
 export const StatusIcon = ({ status, ...props }) => {
@@ -10,18 +11,21 @@ export const StatusIcon = ({ status, ...props }) => {
     COMMENTED: ChatBubbleRoundedIcon,
     CHANGES_REQUESTED: RateReviewRoundedIcon,
     APPROVED: CheckRoundedIcon,
+    PENDING: AccessTimeRoundedIcon,
   }[status];
 
   const bgClass = {
     COMMENTED: `bg-white`,
     CHANGES_REQUESTED: `bg-red-500`,
     APPROVED: `bg-green-600`,
+    PENDING: `bg-yellow-500`,
   }[status];
 
   const textClass = {
     COMMENTED: `text-gray-600`,
     CHANGES_REQUESTED: `text-red-100`,
     APPROVED: `text-green-100`,
+    PENDING: `text-yellow-900`,
   }[status];
 
   return (
