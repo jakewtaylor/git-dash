ac2c9e1a330a1032f65f01a707575abd804168b9
Jake Taylor <jake.taylor@pickr.works>
2020-02-26 17:15:42 +0000

    switch to inter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
       inset: {
         'full': '100%',
         '1/2': '50%',
